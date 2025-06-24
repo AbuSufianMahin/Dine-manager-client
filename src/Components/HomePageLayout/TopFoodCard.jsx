170350f37841adcf9d73727b0150b7429769008d
Abu Sufian Mahin <abusufianmahin3@gmail.com>
2025-06-24 19:44:09 +0600

    added Top Foods Section

---
+++ src/Components/HomePageLayout/TopFoodCard.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+const TopFoodCard = ({ foodData }) => {
+    console.log(foodData);
+
+    const { _id, foodName, foodImageURL, foodOrigin, price, totalSold, foodCategory } = foodData;
+    return (
+        <div
+            className='shadow-md border border-secondary p-5 rounded-lg'>
+
+            <div className='h-full flex flex-col justify-between'>
+                <img src={foodImageURL} alt={foodName} className='w-full rounded-xl' />
+
+                <div className='flex justify-between items-center mt-5'>
+                    <div className='flex flex-col md:flex-row md:items-center gap-2'>
+                        <Link to={`/food-details/${_id}`}><h3 className='text-xl font-bold transition duration-200 hover:text-primary '>{foodName}</h3></Link>
+                        <p className='text-center rounded-xl bg-sky-400 w-fit px-2'>{foodCategory}</p>
+                    </div>
+                    <p className='font-semibold'>{price} BDT</p>
+                </div>
+
+                <div className='my-3'>
+                    <p>Total Sold x {totalSold}</p>
+                    <p>Origin : {foodOrigin}</p>
+                </div>
+                <div className='flex gap-2'>
+                    <Link className='flex-1 btn btn-primary text-white' to={`/food-details/${_id}`}>View Details</Link>
+                    <Link className='flex-1 btn btn-primary text-white' to={`/purchase-food/${_id}`}>Order Now</Link>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default TopFoodCard;
--- src/Components/HomePageLayout/TopFoodSection.jsx
+++ src/Components/HomePageLayout/TopFoodSection.jsx
@@ -1,21 +1,29 @@
 import React, { useEffect, useState } from 'react';
+import TopFoodCardContainer from './TopFoodCard';
+import TopFoodCard from './TopFoodCard';
 
 const TopFoodSection = () => {
     const [topFoods, setTopFoods] = useState([]);
-    
-    useEffect(()=>{
-        fetch()
-    })
+
+    useEffect(() => {
+        fetch('http://localhost:3000/top-food-details')
+            .then(res => res.json())
+            .then(data => setTopFoods(data))
+    },[])
 
     return (
         <section className='py-10 lg:py-20'>
-            <div className='w-11/12 md:w-10/12 mx-auto'>
+            <div className='w-11/12 md:w-10/12 lg:w-8/12 mx-auto'>
                 <div className='text-center space-y-2'>
                     <h1 className='text-center text-3xl md:text-4xl font-bold'>Customer Favorites</h1>
                     <p className='md:text-lg'>Tried, tested, and loved. These are the meals our customers can't get enough of — now it's your turn to dig in!</p>
                 </div>
 
-
+                <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-3 mt-10'>
+                    {
+                        topFoods.map(foodData => <TopFoodCard key={foodData._id} foodData={foodData}></TopFoodCard>)
+                    }
+                </div>
             </div>
         </section>
     );
