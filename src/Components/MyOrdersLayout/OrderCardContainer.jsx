8ad308e939867d5e1c4e6576bc210253b74bd12a
Abu Sufian Mahin <abusufianmahin3@gmail.com>
2025-08-11 09:36:40 +0600

    fixed active orders responsiveness

--- src/Components/MyOrdersLayout/OrderCardContainer.jsx
+++ src/Components/MyOrdersLayout/OrderCardContainer.jsx
@@ -5,7 +5,7 @@ const OrderCardContainer = ({ allOrders, setAllOrders }) => {
     return (
         <div className='min-h-[35vh]'>
             <h1 className='text-center text-3xl md:text-4xl font-bold'>Active orders</h1>
-            <div className='mt-5 md:mt-10 grid grid-cols-2'>
+            <div className='mt-5 md:mt-10 grid gap-4 grid-cols-1 lg:grid-cols-2'>
                 {
                     allOrders.map(orderData => <OrderCard key={orderData.orderId} orderData={orderData} allOrders={allOrders} setAllOrders={setAllOrders}></OrderCard>)
                 }
